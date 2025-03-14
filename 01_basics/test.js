e5ed76310c9fb581484cac55db1dbc746dec03e4
Payal Sharma <coderpayal7895@gmail.com>
2025-03-14 15:56:08 +0000

    container change done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,26 @@
-console.log("Hello world")
+// First Code of Java Script    --------------------------------  " HEllo World!"  ------------------------------------
+
+// console.log("Hello World!")
+
+//  -----------------------------------------------------   let Const or Variable ------------------------------------------
+
+const acc_email = "ys286568@gimal.com"
+// const acc_email = "yuktisharma@gmail.com"   cannot be reassign or redeclared 
+console.log(acc_email)
+
+let acc_id = 14452
+// let acc_id = 56647       can be  reassign not redeclared
+console.log(acc_id)
+
+var acc_logged_in = true
+//  acc_logged_in = false       can be  reassign or redeclared
+console.log(acc_logged_in)
+
+
+user_name = "Payal Sharma"
+user_name = "Yukti Sharma"
+console.log(user_name)
+
+let acc_no;
+
+console.table([acc_email, acc_id, acc_logged_in , user_name, acc_no])
