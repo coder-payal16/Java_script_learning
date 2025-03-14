3c2fa4a841f409ce104d6c13bd7d0b1216388321
Payal Sharma <coderpayal7895@gmail.com>
2025-03-14 16:16:02 +0000

    data type done

---
+++ 01_basics/data_type.js
@@ -0,0 +1,29 @@
+// ---------------------------------------------------------- Data Type -------------------------------------------------------
+
+//   -------String Type-------
+let name = "Payal Sharma";
+console.log(name)
+console.log(typeof name)   
+
+//   -------Number Type-------
+let age = 0.21
+console.log(age)
+console.log(typeof age)  
+
+//   -------Boolean Type-------
+let logged_out = true
+console.log(logged_out)
+console.log(typeof logged_out) 
+
+
+//   -------null Type-------
+let value = null
+console.log(value)
+console.log(typeof value) 
+
+//   -------undefined Type-------
+let value_defined = undefined
+console.log(value_defined)
+console.log(typeof value_defined) 
+
+
