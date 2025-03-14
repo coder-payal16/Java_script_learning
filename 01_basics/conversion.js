0f413f675cd1c16bb0abff9189c2de65bac173ab
Payal Sharma <coderpayal7895@gmail.com>
2025-03-14 16:42:27 +0000

    conversion done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,26 @@
+//-------------------------------------- Convert into number ----------------------------------------------
+// const value = " "
+// const new_value = Number(value)
+// console.log(new_value)
+// console.log(typeof new_value)
+
+
+// const name = 'payal'
+// const new_name = Number(name)
+// console.log(new_name)
+// console.log(typeof new_name)
+
+// const booltype = false
+// const check_num = Number(booltype)
+// console.log(check_num)
+// console.log(typeof check_num)
+
+// const nulltype = null
+// const check_null = Number(nulltype)
+// console.log(check_null)
+// console.log(typeof check_null)
+
+const definedtype = undefined
+const check_defined = Number(definedtype)
+console.log(check_defined)
+console.log(typeof check_defined)
