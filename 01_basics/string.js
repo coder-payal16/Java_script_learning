90f5264ff391c16fab6f8f3622797b2b130acaae
Payal Sharma <coderpayal7895@gmail.com>
2025-03-15 04:14:50 +0000

    string done

---
+++ 01_basics/string.js
@@ -0,0 +1,15 @@
+//  ---------------------------------string -------------------------------------------
+console.log("Payal Sharma")
+const college_name = "Gagan College of Management  and Technology";
+const student = 12546;
+console.log(college_name +  student)
+console.log(`Youe College name is ${college_name} , students in your college ${student}`);
+
+const gameName = new String("Yukti Saraswat")
+console.log(gameName)
+// console.log(typeof gameName)
+console.log(gameName[3]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+console.log(gameName.charAt(5))
